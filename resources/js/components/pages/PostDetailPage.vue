5fae6fed23fbd6818764a8581aa46fe51bfc5d9e
Damianp99 <damianperez200199@gmail.com>
2022-04-06 17:29:46 +0200

    added page.show

---
+++ resources/js/components/pages/PostDetailPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>detail page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostDetailPage",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
