cdcd96716934108d179272230801a4a08214c475
Damianp99 <damianperez200199@gmail.com>
2022-04-06 17:27:53 +0200

    404 page

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      not-found
+      d-flex
+      flex-column
+      justify-content-center
+      align-items-center
+    "
+  >
+    <h2 class="text-muted text-center my-5">404 | Not Found</h2>
+    <button class="btn btn-secondary" @click="$router.back()">Indietro</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style>
+</style>
