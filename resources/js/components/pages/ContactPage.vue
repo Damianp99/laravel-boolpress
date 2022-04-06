cda47223a18f31218be9b21f6ad69506fbd9dde5
Damianp99 <damianperez200199@gmail.com>
2022-04-06 16:16:01 +0200

    added  new pages

---
+++ resources/js/components/pages/ContactPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>Contattaci</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci optio ex
+      quos laboriosam omnis quidem commodi tempore ipsa maiores mollitia eaque
+      veniam aliquam labore, beatae natus excepturi, rerum enim magni. Magni
+      voluptas consectetur, quidem, facilis non porro pariatur odio accusamus,
+      delectus hic sed! Similique, qui voluptatum odio ut architecto minus
+      impedit voluptate autem necessitatibus? Nulla iusto voluptas facilis sit
+      dolore?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactPage",
+  components: {},
+};
+</script>
+
+<style>
+</style>
---
+++ resources/js/components/pages/Homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <PostIndex />
+  </div>
+</template>
+
+<script>
+import PostIndex from "../posts/PostIndex.vue";
+export default {
+  name: "Homepage",
+  components: {
+    PostIndex,
+  },
+};
+</script>
+
+<style>
+</style>
